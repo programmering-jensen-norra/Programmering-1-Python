95e0cd398189ab5203e47598ee2e4e4555e7d336
Matej Buljan <matej.buljan@jenseneducation.se>
2023-02-13 12:16:00 +0100

    Changed hello and variables

--- hello.py
+++ hello.py
@@ -5,5 +5,5 @@ print("the " + world_name + " is beautiful ")
 world_name = "space"
 print("Perfect " + world_name )
 
-phrase = "good morning"
+phrase = "good evening"
 print(len(phrase))
--- variables.py
+++ variables.py
@@ -6,6 +6,3 @@ print(value)
 
 value = "19" + 2
 print(value)
-
-value = 19 + "2"
-print(value)
